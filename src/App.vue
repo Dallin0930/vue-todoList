ffcc47230868b3d22dbfa81f374f59a64eebaece
Dallin0930 <2869865192@qq.com>
2019-07-21 12:39:21 +0800

    [dallin]:feat_implement_select_and_filter

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,76 @@
 
 <template>
-<div id="container">
-  <div id="app">   
-        <div>
-            <h2>Jquery To Do List</h2>
-            <p>
-                <em>Simple Todo List with adding and filter by diff status.</em>
-            </p>
-        </div>  
+  <div class="container">
+      <div>
+          <h2>Jquery To Do List</h2>
+          <p>
+              <em>Simple Todo List with adding and filter by diff status.</em>
+          </p>
+      </div>
+      <div>
+        <input v-model="inputValue" class="input-text" name="ListItem" />
 
-        <div>
-            <input id="input-text" type="text" name="ListItem"  v-model="list"/>&nbsp;&nbsp;     
-            <button id="buttonAdd" v-on:click="addList">add</button>
-        </div>
-        <br/>
-
+        <button id="button" @click="add">add</button>
         <ol>
-            
-            <li v-for="(d,index) in counter" :key="index">
-                <input type="checkbox">
-                <input type="text"  placeholder="0">
-            </li> 
-            
-             
+        <li v-for="(item,index) in checkboxes" :key="index">
+            <input type="checkbox" placeholder="0"   v-model="item.isChecked">
+            <span>{{item.checkBoxValue}}</span>
+        </li> 
         </ol>
+      </div>
+      <br/>
+      <div>
+          <ul id="filters">
+              <li>
+                  <a href="#" data-filter="all" @click="showAll">ALL</a>
+              </li>
+              <li>
+                  <a href="#" data-filter="active" @click="showUnchecked">Active</a>
+              </li>
+              <li>
+                  <a href="#" data-filter="complete" @click="showChecked">Complete</a>
+              </li>
+          </ul>
 
-        
-        <div>                    
-            <button id="buttonOne">All</button>
-            <button id="buttonTwo">Active</button>  
-            <button id="buttonThree">Complete</button>  
-        </div>
+      </div>
   </div>
-</div>
-</template> 
-
-
+</template>
 
 
 <script>
-    export default {
-        name:'App',
-        data(){
-            return {
-                counter:[]
-            }
-        },
-        methods: {  
-            addList(){
-                this.counter.push({});
-            }           
-        }
-    }
+export default {
+  name:'App',
+    data(){
+      return {
+        inputValue:'',
+        checkboxes:[]
+      }
+    },
+    methods: { 
+      add(){ 
+        this.checkboxes.push({checkBoxValue:this.inputValue,isChecked:false});
+        this.checkboxesSlice=this.checkboxes.slice(); 
+        this.inputValue =''
+      },
+      showAll(){
+        this.checkboxes
+      },
+      showUnchecked(){
+        this.checkboxes = this.checkboxesSlice.filter(item => item.isChecked==true)
+      }
+      ,
+      showChecked(){
+        this.checkboxes = this.checkboxesSlice.filter(item => item.isChecked==false)
+      }
+  }
+}
 </script>
+
+
 
 
 <style>
   @import './styles/todoList.css'; 
 </style>
+
+
---
+++ src/components/todoList.vue
@@ -0,0 +1,63 @@
+
+<template>
+<div id="container">
+  <div id="app">   
+        <div>
+            <h2>Jquery To Do List</h2>
+            <p>
+                <em>Simple Todo List with adding and filter by diff status.</em>
+            </p>
+        </div>  
+
+
+        <div>
+            <input id="input-text" type="text" name="ListItem"  v-model="inputValue"/>&nbsp;&nbsp;     
+            <button id="buttonAdd" v-on:click="addList">add</button>  
+        </div>
+        <br/>
+
+
+
+        <ol>           
+            <li v-for="(item,index) in checkboxes" :key="index">
+                <input type="checkbox" placeholder="0" v-model="item.isChecked">
+                <span>{{item.checkboxValue}}</span>        
+            </li> `                         
+        </ol>
+
+        
+        <div>                    
+            <button id="buttonOne">All</button>      
+            <button id="buttonTwo">Active</button>  
+            <button id="buttonThree">Complete</button>  
+        </div>
+  </div>
+</div>
+</template> 
+
+
+
+
+<script>
+    export default {
+        name:'todoList',
+        data(){
+            return {
+                inputValue:'',
+                checkboxes:[]
+            }
+        },
+        methods: {  
+            addList(){
+                this.checkboxes.push({checkBoxValue:this.inputValue,isChecked:false});
+                this.checkboxesSlice=this.checkboxes.slice();
+                this.inputValue =''
+            }           
+        }  
+    }   
+</script>
+
+
+<style>
+  @import '../styles/todoList.css'; 
+</style>
